41dec819ed9c76045f4dff1654cffb703e047818
mohammadruman <mohammaderuman@gmail.com>
2024-12-24 00:36:51 +0530

    add: files to git

---
+++ app/components/hello.tsx
@@ -0,0 +1,10 @@
+"use client";
+function Hello(){
+    console.log("this is a client side ");
+    return(
+    <div>
+    <h1>hello this is a client side </h1>
+    </div>
+    );
+}
+export default Hello;
