9c374a3c9c81f05e616a894d593f10ba26a431dd
mohammadruman <mohammaderuman@gmail.com>
2025-02-05 03:06:12 +0530

    feat: NextAuth GitHub auth

--- app/component/Navbar.tsx
+++ app/component/Navbar.tsx
@@ -1,17 +1,44 @@
-import React from 'react'
+import React, { use } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
+import { auth, signOut, signIn } from '@/auth'
 
-const navbar = () => {
+const Navbar = async () => {
+  const session = await auth();
   return (
-   <header className='px-5 py-3 bg-white shadow-sm font-work-sans'>
-    <nav className='flex justify-between items-center'>
-    <Link href=""> 
-    <Image src="/logo.png" alt="logo" width={144} height={30}></Image>
-    </Link>
-    </nav>
-   </header>
+    <header className='px-5 py-3 bg-white shadow-sm font-work-sans'>
+      <nav className='flex justify-between items-center'>
+        <Link href="">
+          <Image src="/logo.png" alt="logo" width={144} height={30} />
+        </Link>
+        <div className='flex items-center gap-5 text-black'>
+          {session && session.user ? (
+            <>
+              <Link href="/startup/create">
+                <span>Create</span>
+              </Link>
+              <form action={async()=>{
+                "use server";
+                await signOut({redirectTo:"/"});
+              }}>
+                <button type='submit' >Logout</button>
+              </form>
+              <Link href={`/user/${session.id}`}>
+                <span>{session.user.name}</span>
+              </Link>
+            </>
+          ) : (
+            <form action={async () => {
+              "use server";
+              await signIn({ provider: 'github' });
+            }}>
+              <button type='submit'>Login</button>
+            </form>
+          )}
+        </div>
+      </nav>
+    </header>
   )
 }
 
-export default navbar
+export default Navbar
