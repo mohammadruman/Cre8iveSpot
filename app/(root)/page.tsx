ecb4cc82540b16418ac962af21c9af0a0ac8612a
mohammadruman <mohammaderuman@gmail.com>
2025-02-05 05:38:07 +0530

    feat : added search functionality

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,18 @@
 import Image from "next/image";
+import Searchform from "@/components/Searchform";
 
-export default function Home() {
+export default async function Home({searchParams}:{
+  searchParams: promise<{query: string}>
+}) {
+  const query = (await searchParams).query;
   return (
     <>
     <section className="pink_container">
     <h1 className="heading">Your SaaS, Your Impact<br/>Let the World See the Problem You’ve Solved!</h1>
+<p className="sub-heading !max-w-3xl">Get your SaaS application in front of the right audience. </p>
+    <Searchform query={query}/>
     </section>
+   
    
     </>
   );
---
+++ components/Searchform.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import SearchFormResetButton from './SearchFormResetButton'
+import Form from 'next/form'
+import {  Link} from "next/link";
+import { Search } from 'lucide-react';
+
+const Searchform = ({query}:{query?:string}) => {
+   
+  return (
+   <Form action="/" scroll={false} className="search-form">
+    <input 
+    name="query"
+    defaultValue={query}
+    className="search-input"
+    placeholder="Search for SaaS applications"
+    />
+    <div className="flex gap-2">
+        {query && <SearchFormResetButton/>}
+        <button type="submit" className="search-btn text-white"><Search className="size-5"/></button>
+        </div>
+        </Form>
+  )
+}
+
+export default Searchform
