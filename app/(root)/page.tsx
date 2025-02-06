a8c21641702d390ac7883ee2d10c0a211b5a44b8
mohammadruman <mohammaderuman@gmail.com>
2025-02-07 04:25:19 +0530

    fix : saas card component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,42 @@
 import Image from "next/image";
 import Searchform from "@/components/Searchform";
+import StartupCard from "@/components/StartupCard";
 
 export default async function Home({searchParams}:{
   searchParams: promise<{query: string}>
 }) {
+ const posts =[ {
+    "_createdAt": "Yesterday",
+    "views": 55,
+    "author": { "_id": 1 },
+    "_id": 1,
+    "description": "This is a description",
+    "image": "https://images.unsplash.com/photo-1634912314704-c646c586b131?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wdXJlWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "category": "Robots",
+    "title": "We Robots"
+  },
+  {
+    "_createdAt": "Yesterday",
+    "views": 55,
+    "author": { "_id": 1 },
+    "_id": 1,
+    "description": "This is a description",
+    "image": "https://images.unsplash.com/photo-1634912314704-c646c586b131?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wdXJlWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "category": "Robots",
+    "title": "We Robots"
+  },
+  {
+    "_createdAt": "Yesterday",
+    "views": 55,
+    "author": { "_id": 1 },
+    "_id": 1,
+    "description": "This is a description",
+    "image": "https://images.unsplash.com/photo-1634912314704-c646c586b131?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wdXJlWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "category": "Robots",
+    "title": "We Robots"
+  }
+];
+  
   const query = (await searchParams).query;
   return (
     <>
@@ -18,7 +51,12 @@ export default async function Home({searchParams}:{
 
     </p>
     <ul className="mt-7 card_grid">
-
+      {posts?.length >0 ? (posts.map((post : StartupCardType , index :number)=>(
+        <StartupCard/> 
+      ))
+    ) :(
+      <p className="no-result">No Saas Found</p>
+    )}
     </ul>
     </section>
    
